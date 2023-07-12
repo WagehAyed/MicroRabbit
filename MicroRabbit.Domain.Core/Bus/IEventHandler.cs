14c77adc2b85365d843c748eb22c3f651ef4a6d0
Wageh Ayed <wageh.ayed@gmail.com>
2023-07-13 02:17:43 +0300

    Add project files.

---
+++ MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in Tevent> : IEventHandler
+        where Tevent : Event
+    {
+        Task Handle(Tevent @event);
+
+    }
+
+   public interface IEventHandler
+    {
+
+    }
+}
