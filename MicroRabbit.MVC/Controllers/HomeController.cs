37e6a508856c926850749599bf6cc1b918e1747e
Wageh Ayed <wageh.ayed@gmail.com>
2023-07-15 10:58:30 +0300

    Create mvc Presentation

---
+++ MicroRabbit.MVC/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Mapster;
+using MicroRabbit.MVC.Models;
+using MicroRabbit.MVC.Models.DTO;
+using MicroRabbit.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+        private readonly ITransferService _transferService;
+        
+        public HomeController(ILogger<HomeController> logger, ITransferService transferService)
+        {
+            _logger = logger;
+            _transferService = transferService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost("Transfer")]
+        public async Task<IActionResult> Transfer(TransferVm viewModel)
+        {
+            TransferDTO TransferDTO = viewModel.Adapt<TransferDTO>();
+            await _transferService.Transfer(TransferDTO);
+            return View("Index");
+        }
+    }
+}
---
+++ MicroRabbit.MVC/Models/TransferVm.cs
@@ -0,0 +1,10 @@
+﻿namespace MicroRabbit.MVC.Models
+{
+    public class TransferVm
+    {
+        public string TranferNotes { get; set; }
+        public int FromAccount { get; set; }
+        public int ToAccount { get; set; }
+        public decimal TransferAmount { get; set; }
+    }
+}
